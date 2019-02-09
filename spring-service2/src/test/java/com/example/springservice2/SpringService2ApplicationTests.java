01346848f37ead308239b7f81deb698c026875f7
sathi2 <sathi@wired2perform.com>
2019-02-09 22:21:32 +0530

    submodules addded

--- spring-service2/src/test/java/com/example/springservice2/SpringService2ApplicationTests.java
+++ spring-service2/src/test/java/com/example/springservice2/SpringService2ApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.jxspringdemo;
+package com.example.springservice2;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class DemoApplicationTests {
+public class SpringService2ApplicationTests {
 
 	@Test
 	public void contextLoads() {
